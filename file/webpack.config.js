f855c29f292cc6d9620d8cd126163ba472ac0ed3
kenblikylee <kenblikylee@126.com>
2020-02-04 19:59:58 +0800

    file-loader options outputPath

--- file/webpack.config.js
+++ file/webpack.config.js
@@ -12,7 +12,10 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader'
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images'
+            }
           }
         ]
       }
