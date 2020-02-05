a1f70a3cd295fdaf4c9b0085ebfa44c1ff1c7d23
kenblikylee <kenblikylee@126.com>
2020-02-05 17:21:32 +0800

    babel config add eslint

--- babel/webpack.config.js
+++ babel/webpack.config.js
@@ -3,17 +3,27 @@ const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin
 
 module.exports = {
   devtool: 'source-map',
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.m?js$/i,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          },
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true,
+              cache: true
+            }
           }
-        }
+        ]
       }
     ]
   },
