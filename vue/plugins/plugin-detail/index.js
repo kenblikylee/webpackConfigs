5347825931d5939d236c7e61388a979a72eb1e05
kenblikylee <kenblikylee@126.com>
2020-02-22 16:13:36 +0800

    simplify detail plugin

--- vue/plugins/plugin-detail/index.js
+++ vue/plugins/plugin-detail/index.js
@@ -1,20 +1,14 @@
-const Component = {
-  functional: true,
-  render(h, { props }) {
-    return <div>
-            <h2>Detail</h2>
-            <p>detail { props.id }</p>
-          </div>
+export default ({ router }) => router.addRoutes({
+  name: 'detail',
+  path: '/detail/:id',
+  props: true,
+  component: {
+    functional: true,
+    render(h, { props }) {
+      return <div>
+              <h2>Detail</h2>
+              <p>detail { props.id }</p>
+            </div>
+    }
   }
-}
-
-const routes = [
-  {
-    name: 'detail',
-    path: '/detail/:id',
-    component: Component,
-    props: true
-  }
-]
-
-export default ({ router }) => router.addRoutes(routes)
+})
